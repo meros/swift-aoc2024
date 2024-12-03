72e081ba428d3ece6aed5b1b76c1ab93a9e453b2
Alexander Schrab <alexander.schrab@gmail.com>
2024-12-03 17:35:34 +0100

    Cleanups, shorten

--- Sources/Day03/Day03.swift
+++ Sources/Day03/Day03.swift
@@ -1,12 +1,19 @@
 import DayUtils
 import Foundation
 
-let pattern = #/mul\(([0-9]{1,3}),([0-9]{1,3})\)|do\(\)|don't\(\)/#
+typealias PatternType = Regex<
+    (
+        Substring, mul: Substring?, factor1: Substring?, factor2: Substring?, do: Substring?,
+        dont: Substring?
+    )
+>
+let pattern: PatternType =
+    #/(?<mul>mul\((?<factor1>[0-9]{1,3}),(?<factor2>[0-9]{1,3})\))|(?<do>do\(\))|(?<dont>don't\(\))/#
 
-func getProduct(_ match: Regex<Regex<(Substring, Substring?, Substring?)>.RegexOutput>.Match) -> Int
-{
-    if let factor1Substring = match.output.1,
-        let factor2Substring = match.output.2,
+func getProduct(_ match: PatternType.Match) -> Int {
+    if match.mul != nil,
+        let factor1Substring = match.factor1,
+        let factor2Substring = match.factor2,
         let factor1 = Int(factor1Substring),
         let factor2 = Int(factor2Substring)
     {
@@ -16,32 +23,22 @@ func getProduct(_ match: Regex<Regex<(Substring, Substring?, Substring?)>.RegexO
     return 0
 }
 
+func getZero(_ _: PatternType.Match) -> Int {
+    return 0
+}
+
 public struct Day03: Day {
     public static func solvePart1(_ input: String) -> Int {
-        input.matches(of: pattern).reduce(into: 0) { sum, match in
-            sum += getProduct(match)
-        }
+        input.matches(of: pattern).map(getProduct).reduce(0, +)
     }
 
     public static func solvePart2(_ input: String) -> Int {
-        let matches = input.matches(of: pattern)
-
-        var enabled = true
-        var totalSum = 0
-        for match in matches {
-            let command = match.output.0
-            switch command {
-            case "do()":
-                enabled = true
-            case "don't()":
-                enabled = false
-            default:
-                if enabled {
-                    totalSum += getProduct(match)
-                }
-            }
-        }
-
-        return totalSum
+        input.matches(of: pattern).reduce(
+            into: (prod: getProduct, sum: 0)
+        ) { result, match in
+            result.prod = match.do != nil ? getProduct : result.prod
+            result.prod = match.dont != nil ? getZero : result.prod
+            result.sum += result.prod(match)
+        }.sum
     }
 }
