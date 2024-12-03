9abe9c8cb5b5f77a467474bd1f7a42d458a814d3
Alexander Schrab <alexander.schrab@gmail.com>
2024-12-03 12:00:44 +0100

    Clean up/modernize

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "AdventOfCode2024",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v13)
     ],
     products: [
         .executable(
--- Sources/Day03/Day03.swift
+++ Sources/Day03/Day03.swift
@@ -1,15 +1,16 @@
 import DayUtils
 import Foundation
 
-let pattern = #"mul\(([0-9]{1,3}),([0-9]{1,3})\)|do\(\)|don't\(\)"#
+let pattern = #/mul\(([0-9]{1,3}),([0-9]{1,3})\)|do\(\)|don't\(\)/#
 
-func getProduct(match: NSTextCheckingResult, input: String) -> Int {
-    if let range1 = Range(match.range(at: 1), in: input),
-        let range2 = Range(match.range(at: 2), in: input),
-        let num1 = Int(input[range1]),
-        let num2 = Int(input[range2])
+func getProduct(_ match: Regex<Regex<(Substring, Substring?, Substring?)>.RegexOutput>.Match) -> Int
+{
+    if let factor1Substring = match.output.1,
+        let factor2Substring = match.output.2,
+        let factor1 = Int(factor1Substring),
+        let factor2 = Int(factor2Substring)
     {
-        return num1 * num2
+        return factor1 * factor2
     }
 
     return 0
@@ -17,43 +18,26 @@ func getProduct(match: NSTextCheckingResult, input: String) -> Int {
 
 public struct Day03: Day {
     public static func solvePart1(_ input: String) -> Int {
-        let regex = try! NSRegularExpression(pattern: pattern, options: [])
-        let matches = regex.matches(
-            in: input, options: [], range: NSRange(input.startIndex..., in: input))
-
-        var totalSum = 0
-
-        for match in matches {
-            totalSum += getProduct(match: match, input: input)
+        input.matches(of: pattern).reduce(into: 0) { sum, match in
+            sum += getProduct(match)
         }
-
-        return totalSum
     }
 
     public static func solvePart2(_ input: String) -> Int {
-        let regex = try! NSRegularExpression(pattern: pattern, options: [])
-        let matches = regex.matches(
-            in: input, options: [], range: NSRange(input.startIndex..., in: input))
+        let matches = input.matches(of: pattern)
 
         var enabled = true
         var totalSum = 0
         for match in matches {
-            if let range = Range(match.range(at: 0), in: input) {
-                let matchString = input[range]
-                switch matchString {
-                case "do()":
-                    enabled = true
-                    break
-                case "don't()":
-                    enabled = false
-                    break
-                default:
-                    if !enabled {
-                        break
-                    }
-
-                    totalSum += getProduct(match: match, input: input)
-                    break
+            let command = match.output.0
+            switch command {
+            case "do()":
+                enabled = true
+            case "don't()":
+                enabled = false
+            default:
+                if enabled {
+                    totalSum += getProduct(match)
                 }
             }
         }
