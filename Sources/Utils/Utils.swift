c4339308b72a4e4650aa084219af65da227e6c5a
Alexander Schrab <alexander.schrab@gmail.com>
2024-12-05 21:24:20 +0100

    Day 6 preparation

--- Sources/Utils/Utils.swift
+++ Sources/Utils/Utils.swift
@@ -5,26 +5,43 @@ public protocol Day {
   static func solvePart2(_ input: String) -> Int
 }
 
-public func getInput(_ day: Int, _ session: String) async throws -> String {
-  let cacheURL = URL(fileURLWithPath: ".cache/day\(day).txt")
-  if let cachedInput = try? String(contentsOf: cacheURL, encoding: .utf8) {
-    return cachedInput
+public func getInput(_ day: Int, _ session: String) async -> String? {
+  do {
+    let cacheURL = URL(fileURLWithPath: ".cache/day\(day).txt")
+    if let cachedInput = try? String(contentsOf: cacheURL, encoding: .utf8) {
+      return cachedInput
+    }
+
+    var request = URLRequest(url: URL(string: "https://adventofcode.com/2024/day/\(day)/input")!)
+    request.setValue("session=\(session)", forHTTPHeaderField: "Cookie")
+    let (data, response) = try await URLSession.shared.data(for: request)
+
+    guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+      throw URLError(.badServerResponse)
+    }
+
+    let input = String(data: data, encoding: .utf8)!
+
+    try FileManager.default.createDirectory(atPath: ".cache", withIntermediateDirectories: true)
+    try input.write(to: cacheURL, atomically: true, encoding: .utf8)
+
+    return input
+  } catch {
+    print("Failed to get input for day \(day): \(error)")
+    return nil
   }
+}
 
-  var request = URLRequest(url: URL(string: "https://adventofcode.com/2024/day/\(day)/input")!)
-  request.setValue("session=\(session)", forHTTPHeaderField: "Cookie")
-  let (data, response) = try await URLSession.shared.data(for: request)
-
-  guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-    throw URLError(.badServerResponse)
+public func getExampleInput(_ day: Int) -> String? {
+  let fileURL = URL(fileURLWithPath: ".cache/day\(day).example.txt")
+  // Read file into a string if exists, otherwise create empty template file
+  if let exampleInput = try? String(contentsOf: fileURL, encoding: .utf8), !exampleInput.isEmpty {
+    return exampleInput
+  } else {
+    let template = ""
+    try? template.write(to: fileURL, atomically: true, encoding: .utf8)
+    return nil
   }
-
-  let input = String(data: data, encoding: .utf8)!
-
-  try FileManager.default.createDirectory(atPath: ".cache", withIntermediateDirectories: true)
-  try input.write(to: cacheURL, atomically: true, encoding: .utf8)
-
-  return input
 }
 
 extension [[Int]] {
---
+++ Sources/template/Solution.swift
@@ -0,0 +1,12 @@
+import Foundation
+import Utils
+
+public struct Solution: Day {
+  public static func solvePart1(_ input: String) -> Int {
+    0
+  }
+
+  public static func solvePart2(_ input: String) -> Int {
+    0
+  }
+}
