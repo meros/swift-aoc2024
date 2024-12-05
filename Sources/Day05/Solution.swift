b64aa367ffc4e839bbedd6c1eb70cb7fd279386f
Alexander Schrab <alexander.schrab@gmail.com>
2024-12-05 14:32:54 +0100

    Clean up Day 5

--- Sources/Day05/Solution.swift
+++ Sources/Day05/Solution.swift
@@ -1,50 +1,53 @@
 import Foundation
 import Utils
 
-func parsedInput(_ input: String) -> ([[Int]], [(Int, Int)]) {
+struct PageOrderRule: Hashable {
+  let first: Int
+  let second: Int
+}
+
+typealias PageOrderRules = Set<PageOrderRule>
+typealias PageUpdates = [[Int]]
+
+func parseInput(_ input: String) -> (PageUpdates, PageOrderRules) {
   let parts = input.split(separator: "\n\n")
 
-  let rules = parts.first!.split(separator: "\n").map {
-    $0.split(separator: "|").compactMap { Int($0) }
-  }.map { rule in
-    (rule[0], rule[1])
-  }
+  let rules: PageOrderRules = Set(
+    parts.first!.split(separator: "\n").compactMap {
+      let rule = $0.split(separator: "|").compactMap { Int($0) }
+      return rule.count == 2 ? PageOrderRule(first: rule[0], second: rule[1]) : nil
+    })
 
-  let updates = parts.last!.split(separator: "\n").map {
+  let updates: PageUpdates = parts.last!.split(separator: "\n").map {
     $0.split(separator: ",").compactMap { Int($0) }
   }
 
   return (updates, rules)
 }
 
-func isValidUpdate(_ update: [Int], _ rules: [(Int, Int)]) -> Bool {
+func isValidUpdate(_ update: [Int], _ rules: PageOrderRules) -> Bool {
   zip(update, update.dropFirst()).allSatisfy(getSortingPredicate(rules))
 }
 
-func getSortingPredicate(_ rules: [(Int, Int)]) -> (Int, Int) -> Bool {
-  { page1, page2 -> Bool in rules.contains { rule in rule == (page1, page2) } }
+func getSortingPredicate(_ rules: PageOrderRules) -> (Int, Int) -> Bool {
+  { page1, page2 in rules.contains(PageOrderRule(first: page1, second: page2)) }
 }
 
 public struct Solution: Day {
   public static func solvePart1(_ input: String) -> Int {
-    let (updates, rules) = parsedInput(input)
+    let (updates, rules) = parseInput(input)
 
-    return updates.filter { update in
-      isValidUpdate(update, rules)
-    }.map { validUpdate in
-      validUpdate[(validUpdate.count - 1) / 2]
-    }.reduce(0, +)
+    return updates.filter { isValidUpdate($0, rules) }
+      .map { $0[($0.count - 1) / 2] }
+      .reduce(0, +)
   }
 
   public static func solvePart2(_ input: String) -> Int {
-    let (updates, rules) = parsedInput(input)
+    let (updates, rules) = parseInput(input)
 
-    return updates.filter { update in
-      !isValidUpdate(update, rules)
-    }.map { update in
-      update.sorted(by: getSortingPredicate(rules))
-    }.map { validUpdate in
-      validUpdate[(validUpdate.count - 1) / 2]
-    }.reduce(0, +)
+    return updates.filter { !isValidUpdate($0, rules) }
+      .map { $0.sorted(by: getSortingPredicate(rules)) }
+      .map { $0[($0.count - 1) / 2] }
+      .reduce(0, +)
   }
 }
