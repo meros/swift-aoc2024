af9c93ef6622a583bb120312c9c138f1425ec3a4
Alexander Schrab <alexander.schrab@gmail.com>
2024-12-08 11:14:38 +0100

    More cleanup

--- Sources/Day08/Solution.swift
+++ Sources/Day08/Solution.swift
@@ -1,66 +1,59 @@
 import Foundation
 import Utils
 
-func parseInput(_ input: String) -> ([Character: [Position]], (Int, Int)) {
+public struct Solution: Day {
+  public static func solvePart1(_ input: String) async -> Int {
+    let (antennaGroups, gridSize) = parseAntennaMap(input)
+    return countAntinodes(antennaGroups, gridSize, 1, 1)
+  }
+
+  public static func solvePart2(_ input: String) async -> Int {
+    let (antennaGroups, gridSize) = parseAntennaMap(input)
+    return countAntinodes(antennaGroups, gridSize, 0, max(gridSize.0, gridSize.1))
+  }
+}
+
+func parseAntennaMap(_ input: String) -> ([[Position]], (Int, Int)) {
   let grid = input.split(separator: "\n").map { line in
     line.map { $0 }
   }
 
-  var result: [Character: [Position]] = [:]
+  var antennaPositions: [Character: [Position]] = [:]
 
   for x in 0..<grid.count {
     for y in 0..<grid[0].count {
       if grid[x][y] != "." {
-        var positions = result[grid[x][y]] ?? []
+        var positions = antennaPositions[grid[x][y]] ?? []
         positions.append(Position(x, y))
-        result[grid[x][y]] = positions
+        antennaPositions[grid[x][y]] = positions
       }
     }
   }
 
-  return (result, (grid.count, grid[0].count))
+  return (antennaPositions.map { (_, positions) in positions }, (grid.count, grid[0].count))
 }
 
-public struct Solution: Day {
-  public static func solvePart1(_ input: String) async -> Int {
-    let parsedInput = parseInput(input)
-    return findAntiNodes(parsedInput, 1, 1)
-  }
+func countAntinodes(
+  _ antennaGroups: [[Position]], _ gridSize: (Int, Int), _ minDistance: Int, _ maxDistance: Int
+) -> Int {
+  let (width, height) = gridSize
 
-  public static func solvePart2(_ input: String) async -> Int {
-    let parsedInput = parseInput(input)
-    return findAntiNodes(parsedInput, 0, 100)
+  var antinodes = Set<Position>()
 
-  }
-}
-
-func findAntiNodes(_ parsedInput: ([Character: [Position]], (Int, Int)), _ from: Int, _ to: Int)
-  -> Int
-{
-  let (positionsMap, (width, height)) = parsedInput
-
-  var antiNodes = Set<Position>()
-
-  for (_, positions) in positionsMap {
-    for position in positions {
-      for anotherPosition in positions {
-        if position == anotherPosition {
-          continue
+  for positions in antennaGroups {
+    for (p1, p2) in positions.flatMap({ p1 in positions.filter { $0 != p1 }.map { (p1, $0) } }) {
+      for i in minDistance...maxDistance {
+        let antinodePosition = p2 + (p2 - p1) * i
+        if antinodePosition.x < 0 || antinodePosition.x >= width || antinodePosition.y < 0
+          || antinodePosition.y >= height
+        {
+          break
         }
 
-        for i in from...to {
-          let antiNodePosition = anotherPosition + (anotherPosition - position) * i
-          if antiNodePosition.x < 0 || antiNodePosition.x >= width || antiNodePosition.y < 0
-            || antiNodePosition.y >= height
-          {
-            break
-          }
-          
-          antiNodes.insert(antiNodePosition)
-        }
+        antinodes.insert(antinodePosition)
       }
     }
   }
 
-  return antiNodes.count
+  return antinodes.count
 }
