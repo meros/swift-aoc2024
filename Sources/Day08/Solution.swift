c6ed41d1714d8665a5ef86ca109326560052c80a
Alexander Schrab <alexander.schrab@gmail.com>
2024-12-08 06:48:17 +0100

    Day8

--- Sources/Day08/Solution.swift
+++ Sources/Day08/Solution.swift
@@ -41,6 +41,10 @@ struct Position: PositionProtocol {
   func plus(position: Position) -> Position {
     return Position(x: self.x + position.x, y: self.y + position.y)
   }
+
+  func mult(factor: Int) -> Position {
+    return Position(x: self.x * factor, y: self.y * factor)
+  }
 }
 
 public struct Solution: Day {
@@ -51,21 +55,50 @@ public struct Solution: Day {
 
     var antiNodes = Set<Position>()
 
-    for (char, positions) in parsedInput.0 {
-            for position in positions {
-                let newAntiNodes = positions.filter { $0 != position }
-                    .map {                       $0.plus(position: $0.minus(position: position))                      }
-                    .filter { $0.x >= 0 && $0.x < sizeX && $0.y >= 0 && $0.y < sizeY }
-                for antiNodePosition in newAntiNodes {
-                    antiNodes.insert(antiNodePosition)
-                }
-            }
+    for (_, positions) in parsedInput.0 {
+      for position in positions {
+        let newAntiNodes = positions.filter { $0 != position }
+          .map { $0.plus(position: $0.minus(position: position)) }
+          .filter { $0.x >= 0 && $0.x < sizeX && $0.y >= 0 && $0.y < sizeY }
+        for antiNodePosition in newAntiNodes {
+          antiNodes.insert(antiNodePosition)
         }
-    
+      }
+    }
+
     return antiNodes.count
   }
 
   public static func solvePart2(_ input: String) async -> Int {
-    0
+    let parsedInput = parseInput(input)
+    let sizeX = parsedInput.1.0
+    let sizeY = parsedInput.1.1
+
+    var antiNodes = Set<Position>()
+
+    for (_, positions) in parsedInput.0 {
+      for position in positions {
+        for anotherPosition in positions {
+          if position == anotherPosition {
+            continue
+          }
+
+          let diff = anotherPosition.minus(position: position)
+          for i: Int in 0..<100 {
+            let multDiff = diff.mult(factor: i)
+            let antiNodePosition = anotherPosition.plus(position: multDiff)
+
+            if antiNodePosition.x >= 0 && antiNodePosition.x < sizeX && antiNodePosition.y >= 0 && antiNodePosition.y < sizeY {
+              print()
+              antiNodes.insert(antiNodePosition)
+            } else {
+              break
+            }
+          }
+        }
+      }
+    }
+
+    return antiNodes.count
   }
 }
