4c9c911fb3c3c196abacd0cb0380c920da769953
Alexander Schrab <alexander.schrab@gmail.com>
2024-12-10 17:29:30 +0100

    Clean up naming

--- Sources/Day10/Solution.swift
+++ Sources/Day10/Solution.swift
@@ -2,60 +2,54 @@ import Foundation
 import Utils
 
 func parseInput(_ input: String) -> Grid<Int> {
-  Grid(
-    input.parseGrid().map {
-      $0.map {
-        Int(String($0))!
-      }
-    })
+  Grid(input.parseGrid().compactMap { $0.compactMap { Int(String($0)) } })
 }
 
 func traverseTrail(
   _ grid: Grid<Int>,
-  _ current: Position,
-  _ trailhead: Position,
-  _ onPeakReached: (_ peak: Position, _ trailhead: Position) -> Void
+  from current: Position,
+  trailhead: Position,
+  onPeakReached: (_ trailhead: Position, _ peak: Position) -> Void
 ) {
-  if grid[current] == 9 {
+  guard grid[current] < 9 else {
     onPeakReached(trailhead, current)
-  } else {
-    Direction.allDirections
-      .map { current + $0 }
-      .filter { grid.inBounds($0) && grid[$0] == grid[current] + 1 }
-      .forEach { traverseTrail(grid, $0, trailhead, onPeakReached) }
+    return
   }
+
+  Direction.allDirections
+    .map { current + $0 }
+    .filter { grid.inBounds($0) && grid[$0] == grid[current] + 1 }
+    .forEach { traverseTrail(grid, from: $0, trailhead: trailhead, onPeakReached: onPeakReached) }
 }
 
 public struct Solution: Day {
-  public static var onlySolveExamples: Bool {
-    return false
-  }
+  public static var onlySolveExamples: Bool { false }
 
   public static func solvePart1(_ input: String) async -> Int {
     let grid = parseInput(input)
-    
-    var trailheadToPeaksMap: [Position: Set<Position>] = [:]
+    var trailheadToPeaks: [Position: Set<Position>] = [:]
 
-    grid.forEach {
-      if $1 == 0 {
-        traverseTrail(grid, $0, $0) { trailheadToPeaksMap[$0, default: []].insert($1) }
+    grid.forEach { position, value in
+      guard value == 0 else { return }
+      traverseTrail(grid, from: position, trailhead: position) { trailhead, peak in
+        trailheadToPeaks[trailhead, default: []].insert(peak)
       }
     }
 
-    return trailheadToPeaksMap.values.map { $0.count }.reduce(0, +)
+    return trailheadToPeaks.values.map(\.count).reduce(0, +)
   }
 
   public static func solvePart2(_ input: String) async -> Int {
     let grid = parseInput(input)
+    var trailheadToPathCount: [Position: Int] = [:]
 
-    var trailheadToPathCountMap: [Position: Int] = [:]
-
-    grid.forEach {
-      if $1 == 0 {
-        traverseTrail(grid, $0, $0) { trailheadToPathCountMap[$1, default: 0] += 1 }
+    grid.forEach { position, value in
+      guard value == 0 else { return }
+      traverseTrail(grid, from: position, trailhead: position) { _, _ in
+        trailheadToPathCount[position, default: 0] += 1
       }
     }
 
-    return trailheadToPathCountMap.values.reduce(0, +)
+    return trailheadToPathCount.values.reduce(0, +)
   }
 }
