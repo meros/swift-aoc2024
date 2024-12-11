5fdec0073ee8ffadaf29545f64e27cccf67f9c6b
Alexander Schrab <alexander.schrab@gmail.com>
2024-12-11 12:21:58 +0100

    Day 11 Cleanup

--- Sources/Day11/Solution.swift
+++ Sources/Day11/Solution.swift
@@ -1,65 +1,62 @@
 import Foundation
 import Utils
 
-func parsedInput(_ input: String) -> [Int] {
-  let list = input.split(separator: " ").compactMap { Int($0
-          .replacingOccurrences(of: "\n", with: "")
-
-  ) }
-
-  return list
+func parseInitialStones(_ input: String) -> [Int] {
+  input.matches(of: #/([0-9]+)/#).compactMap { Int($0.output.1) }    
 }
 
 func hasEvenNumberOfDigits(_ number: Int) -> Bool {
-    let digitCount = Int(log10(Double(abs(number)))) + 1
-    return digitCount & 1 == 0 // Bitwise AND for checking evenness
+  let digitCount = Int(log10(Double(abs(number)))) + 1
+  return digitCount & 1 == 0 
 }
 
-func processStone(_ value: Int) -> [Int] {
-  if value == 0 {
-      return  [1]
-    }
+func transformStone(_ stoneNumber: Int) -> [Int] {
+  if stoneNumber == 0 {
+    // Rule 1: Replace 0 with 1
+    return [1]
+  }
     
-    if hasEvenNumberOfDigits(value) {
-      let stringValue = String(value)
-        let midIndex = stringValue.index(stringValue.startIndex, offsetBy: stringValue.count / 2)
-        return [
-          Int(stringValue[stringValue.startIndex..<midIndex])!,
-          Int(stringValue[midIndex..<stringValue.endIndex])!]        
-    }
-
-    return [value * 2024]
-}
-
-func blink(_ stones: [Int]) -> [Int] {
-  stones.flatMap { processStone($0) } 
-}
-
-struct DynamicResultKey: Hashable {
-  var stone: Int
-  var blinksLeft: Int
-}
-
-var dynamicResult: [DynamicResultKey: Int] = [:]
-func blinkOneStoneGetNumberOfStonesCreated(_ stone: Int, _ blinksLeft: Int) -> Int {
-  let dynamicResultKey = DynamicResultKey(stone: stone, blinksLeft: blinksLeft)
-  if let dynamicResult = dynamicResult[dynamicResultKey] {
-    return dynamicResult
+  if hasEvenNumberOfDigits(stoneNumber) {
+    // Rule 2: Split even-digit numbers into two stones
+    let stringValue = String(stoneNumber)
+    let midIndex = stringValue.index(stringValue.startIndex, offsetBy: stringValue.count / 2)
+    return [
+      Int(stringValue[stringValue.startIndex..<midIndex])!,
+      Int(stringValue[midIndex..<stringValue.endIndex])!
+    ]        
   }
 
-  if (blinksLeft == 0) {
+  // Rule 3: Multiply by 2024
+  return [stoneNumber * 2024]
+}
+
+func simulateBlink(_ stones: [Int]) -> [Int] {
+  stones.flatMap { transformStone($0) } 
+}
+
+struct BlinkCacheKey: Hashable {
+  var stoneNumber: Int
+  var blinksRemaining: Int
+}
+
+var blinkCache: [BlinkCacheKey: Int] = [:]
+func countStonesAfterBlinks(_ stoneNumber: Int, _ blinksRemaining: Int) -> Int {
+  if blinksRemaining == 0 {
     return 1
   }
 
-  let blinkedStones = blink([stone])
-  let result = blinkedStones.map {
-    blinkOneStoneGetNumberOfStonesCreated($0, blinksLeft - 1)
+  let key = BlinkCacheKey(stoneNumber: stoneNumber, blinksRemaining: blinksRemaining)
+  if let cachedResult = blinkCache[key] {
+    return cachedResult
+  }
+
+  let totalNewStones = simulateBlink([stoneNumber]).map {
+    countStonesAfterBlinks($0, blinksRemaining - 1)
   }.reduce(0, +)
 
-  dynamicResult[dynamicResultKey] = result
-  return result
+  blinkCache[key] = totalNewStones
+  return totalNewStones
 }
- 
 
 public struct Solution: Day {
   public static var onlySolveExamples: Bool {
@@ -67,18 +64,16 @@ public struct Solution: Day {
   }
 
   public static func solvePart1(_ input: String) async -> Int {
-    let stones = parsedInput(input)
-
-    return stones.map {
-      blinkOneStoneGetNumberOfStonesCreated($0, 25)
+    let initialStones = parseInitialStones(input)
+    return initialStones.map {
+      countStonesAfterBlinks($0, 25)
     }.reduce(0, +)    
   }
 
   public static func solvePart2(_ input: String) async -> Int {
-     let stones = parsedInput(input)
-
-    return stones.map {
-      blinkOneStoneGetNumberOfStonesCreated($0, 75)
+    let initialStones = parseInitialStones(input)
+    return initialStones.map {
+      countStonesAfterBlinks($0, 75)
     }.reduce(0, +)  
   }
 }
