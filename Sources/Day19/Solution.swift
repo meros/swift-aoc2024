6276284c26b221a137b8521b3b285ab6a8f90b8a
Alexander Schrab <alexander.schrab@gmail.com>
2024-12-19 09:01:13 +0100

    Day 19: Cleanup and renames

--- Sources/Day19/Solution.swift
+++ Sources/Day19/Solution.swift
@@ -4,59 +4,61 @@ import Utils
 public struct Solution: Day {
   public static var facitPart1: Int = 240
 
-  public static var facitPart2: Int = 848076019766013
+  public static var facitPart2: Int = 848_076_019_766_013
 
-  public static var onlySolveExamples: Bool {
-    return false
-  }
+  public static var onlySolveExamples: Bool = false
 
   public static func solvePart1(_ input: String) async -> Int {
-    let (regex, patterns, _) = parseInput(input)
-
-    return patterns.filter { $0.wholeMatch(of: regex) != nil }.count
+    let (regex, designs, _) = parseTowelInput(input)
+    return designs.filter { $0.wholeMatch(of: regex) != nil }.count
   }
 
   public static func solvePart2(_ input: String) async -> Int {
-    let (_, patterns, rules) = parseInput(input)
-
-    return patterns.map { solveRecursive(desiredPattern: $0, rules: rules) }.reduce(0, +)
+    let (_, designs, patterns) = parseTowelInput(input)
+    return designs.map { countArrangements(design: $0, patterns: patterns) }.reduce(0, +)
   }
 }
 
-func parseInput(_ input: String) -> (Regex<AnyRegexOutput>, [String], [String]) {
+private func parseTowelInput(_ input: String) -> (Regex<AnyRegexOutput>, [String], [String]) {
   let parts = input.components(separatedBy: "\n\n")
 
-  let rules = parts[0].components(separatedBy: ", ")
-  let rulesRegex = try! Regex("^(\(parts[0].split(separator: ", ").joined(separator: "|")))+$")
-  let messages = parts[1].components(separatedBy: .newlines).filter {
-    !$0.isEmpty
-  }
+  let patterns = parts[0].components(separatedBy: ", ")
+  let patternsRegex = try! Regex("^(\(parts[0].split(separator: ", ").joined(separator: "|")))+$")
+  let designs = parts[1].components(separatedBy: .newlines).filter { !$0.isEmpty }
 
-  return (rulesRegex, messages, rules)
+  return (patternsRegex, designs, patterns)
 }
 
-struct CacheKey : Hashable{
-  let desiredPattern: String
-  let rules: [String]
+private struct TowelArrangementKey: Hashable {
+  let remainingDesign: String
+  let availablePatterns: [String]
 }
 
-var cache = [CacheKey: Int]()
-func solveRecursive(desiredPattern: String, rules: [String]) -> Int {
-  if let cached = cache[CacheKey(desiredPattern: desiredPattern, rules: rules)] {
+private var arrangementCache = [TowelArrangementKey: Int]()
+
+private func countArrangements(design: String, patterns: [String]) -> Int {
+  let key = TowelArrangementKey(remainingDesign: design, availablePatterns: patterns)
+
+  if let cached = arrangementCache[key] {
     return cached
   }
 
-  if desiredPattern.isEmpty {
-    cache[CacheKey(desiredPattern: desiredPattern, rules: rules)] = 1
+  if design.isEmpty {
+    arrangementCache[key] = 1
     return 1
   }
 
-  let result = rules.filter {
-    desiredPattern.hasPrefix($0)
-  }.map {
-    solveRecursive(desiredPattern: String(desiredPattern.dropFirst($0.count)), rules: rules)
-  }.reduce(0, +)
+  let arrangements =
+    patterns
+    .filter { design.hasPrefix($0) }
+    .map {
+      countArrangements(
+        design: String(design.dropFirst($0.count)),
+        patterns: patterns
+      )
+    }
+    .reduce(0, +)
 
-  cache[CacheKey(desiredPattern: desiredPattern, rules: rules)] = result
-  return result
+  arrangementCache[key] = arrangements
+  return arrangements
 }
